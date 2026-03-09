292dbc7c00d0abcc2a6b624c47ebe1c8142057d1
Leyla-la <tranthigiangpltn@gmail.com>
2026-03-09 15:14:20 +0700

    fix(UI): Fix layout, header and sidebar position correctly

--- src/Layout/AdminDashboard.tsx
+++ src/Layout/AdminDashboard.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 
 const AdminDashboard = () => {
     return (
-        <div className='flex'>
+        <div className='flex '>
             <Sidebar />
             <div className='w-full flex flex-col'>
                 <Header />
--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Random from '../Components/Sidebar/Random.tsx'
 import AdminDashboard from '../Layout/AdminDashboard.tsx'
+import LoginPage from '../Pages/LoginPage.tsx'
+import RegisterPage from '../Pages/RegisterPage.tsx'
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
                 <Route path="/" element={<AdminDashboard />}>
                     <Route path="/dashboard" element={<Random />} />
                     <Route path="/pharmacy" element={<Random />} />
